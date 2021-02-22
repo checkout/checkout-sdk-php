5caef60fec94dbc88e2b12762785525e568863c8
alfie-loakes-cko <69196584+alfie-loakes-cko@users.noreply.github.com>
2021-02-22 16:13:37 +0000

    chore: updated boleto example and test

--- tests/Models/Payments/BoletoSourceTest.php
+++ tests/Models/Payments/BoletoSourceTest.php
@@ -3,13 +3,14 @@
 namespace Checkout\tests\Models\Payments;
 
 use Checkout\Models\Payments\BoletoSource;
+use Checkout\Models\Payments\Payer;
 use PHPUnit\Framework\TestCase;
 
 class BoletoSourceTest extends TestCase
 {
     public function testCreate()
     {
-        $model = new BoletoSource('{name}', '{birthdate}', '{cpf}');
+        $model = new BoletoSource('{redirect}', '{country}', new Payer('{name}', '{email}', '{document}'));
         $this->assertEquals($model::MODEL_NAME, $model->type);
     }
 }

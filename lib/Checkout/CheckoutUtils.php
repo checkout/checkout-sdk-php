f0e83563ad0019f1b34b38537b2997884f2ebd5e
Raul Estrela <rsaestrela@gmail.com>
2022-03-10 17:26:17 +0100

    Release 2.0.0-beta4
    * Add support for Risk (Default/Four)
    * Improvements in exception handling
    * Add missing `branch_code` reference to `PaymentBankAccountDestination`
    * Minor fixes in Klarna client (Default/Four)
    * Minor internal refactors

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     public const PROJECT_NAME = "checkout-sdk-php";
-    public const PROJECT_VERSION = "2.0.0-beta3";
+    public const PROJECT_VERSION = "2.0.0-beta4";
 
     public static function formatDate(DateTime $date): string
     {

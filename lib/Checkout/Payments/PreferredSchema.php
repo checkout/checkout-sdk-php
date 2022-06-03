2a2ab806085b7f886f2fc7ec6c24a29058f194d6
Armando Ibarra <aibarrakerzon@gmail.com>
2022-06-02 22:24:22 -0500

    Add cartes bancaries properties to ThreeDsRequest

---
+++ lib/Checkout/Payments/PreferredSchema.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Checkout\Payments;
+
+class PreferredSchema
+{
+    public static $visa = "visa";
+    public static $mastercard = "mastercard";
+    public static $cartes_bancaires = "cartes_bancaires";
+}
---
+++ lib/Checkout/Payments/ProductType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Checkout\Payments;
+
+class ProductType
+{
+    public static $qr_code = "QR Code";
+    public static $in_app = "In-App";
+    public static $official_account = "Official Account";
+    public static $mini_program = "Mini Program";
+}

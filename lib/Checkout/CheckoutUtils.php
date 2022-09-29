5c9292817f017acff6d3a7c5adfe2371d7875525
Armando Ibarra <aibarrakerzon@gmail.com>
2022-09-29 13:43:31 -0500

    Release 3.0.4
    * Added Support for ALMA source (CS2)
    * Added Support for One Klarna source (CS2)
    * Added Support for Fawry source (CS2)
    * Added missing fields for PayPal source (CS2)
    * Added missing fields for Sofort source (CS2)
    * Added scheme to all Session sources (CS2)

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.0.3";
+    const PROJECT_VERSION = "3.0.4";
 
     /**
      * @param DateTime $date

e612c311d8614b8d0e0ede7f9087771961fb022e
Armando Ibarra <aibarrakerzon@gmail.com>
2022-10-12 10:02:29 -0500

    Release 3.0.5
    * Added `upgrade_reason` to 3DS request and response.
    * Removed payment_type: `unscheduled`
    * Added `email_addresses` to ContactDetails (default).
    * Added support for Customer Source (default).

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.0.4";
+    const PROJECT_VERSION = "3.0.5";
 
     /**
      * @param DateTime $date

6daa4ed5ec57ec217ac355bfdb12b8294fd6aeb3
Martin Seco <mseco.93@gmail.com>
2023-02-02 13:09:05 -0300

    Release 3.0.8 * Renames created workflows in tests to add SDK identifier (CS1/CS2) * Added API key auth to Integrated Platforms Balances (CS2) * Added support for Trustly & Cv Connect APM (CS2) * Review payment & payout request schema (CS1/CS2) * Added patch endpoint for payment instruments (CS2) * Update Sepa URL for PPro actions (CS1) * Replace ‘reporting’ scopes with 'reports' on the Reports API (CS2) * Add new Illicado APM (CS2)

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.0.7";
+    const PROJECT_VERSION = "3.0.8";
 
     /**
      * @param DateTime $date

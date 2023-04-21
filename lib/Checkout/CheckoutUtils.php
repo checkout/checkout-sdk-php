861100364301c6b25a0977fb04d07b9ed8999212
Martin Seco <mseco.93@gmail.com>
2023-04-21 11:26:37 -0300

    Release 3.0.12 * Adds `card_token` object to `PaymentInstrumentRequest` for Integrated Platforms (CS2) * Adds `if-match` header to `UpdatePaymentInstrumentRequest` for Integrated Platforms (CS2) * Fix typo for `TransactionCurrencyAmount` in Financial Actions (CS2)

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.0.11";
+    const PROJECT_VERSION = "3.0.12";
 
     /**
      * @param DateTime $date

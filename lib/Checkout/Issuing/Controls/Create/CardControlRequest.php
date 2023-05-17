3125deaeda5e85d205c40624cf8dec2732fb81eb
Martin Seco <mseco.93@gmail.com>
2023-05-17 13:34:13 -0300

    Adds support for Card Control operations [Issuing] (CS2) * Adds support for `createControl` * Adds support for `getCardControls` * Adds support for `getCardControlDetails` * Adds support for `updateCardControl` * Adds support for `removeCardControl`

---
+++ lib/Checkout/Issuing/Controls/Create/CardControlRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Checkout\Issuing\Controls\Create;
+
+abstract class CardControlRequest
+{
+    public function __construct($type)
+    {
+        $this->control_type = $type;
+    }
+
+    /**
+     * @var string value of ControlType
+     */
+    public $control_type;
+
+    /**
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @var string
+     */
+    public $target_id;
+}

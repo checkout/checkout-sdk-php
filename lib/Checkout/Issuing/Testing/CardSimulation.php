dd5c9169897486e6fa79e1db8b9f2e278d66cdbd
Martin Seco <mseco.93@gmail.com>
2023-05-18 10:36:15 -0300

    Adds support for Testing operations [Issuing] (CS2) * Adds support for `simulateAuthorization`

---
+++ lib/Checkout/Issuing/Testing/CardSimulation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Checkout\Issuing\Testing;
+
+class CardSimulation
+{
+    /**
+     * @var string
+     */
+    public $id;
+
+    /**
+     * @var int
+     */
+    public $expiry_month;
+
+    /**
+     * @var int
+     */
+    public $expiry_year;
+}

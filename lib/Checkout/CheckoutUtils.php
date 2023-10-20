2295b0cfdee798068c0e6f061b868ec2a1dd792a
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2023-10-21 00:27:32 +0200

    Release 3.0.17 - Update GitHub Actions yaml files

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.0.16";
+    const PROJECT_VERSION = "3.0.17";
 
     /**
      * @param DateTime $date

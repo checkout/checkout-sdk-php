e8d03bdc2e174de5ddd2e5a48308eb93796805ea
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2023-12-22 15:20:21 +0100

    Release 3.0.19 (#230)
    -Adds request and get `payment contexts` endpoints

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.0.18";
+    const PROJECT_VERSION = "3.0.19";
 
     /**
      * @param DateTime $date

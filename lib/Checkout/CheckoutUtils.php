8e6966a90f13ad2c5d119ad6a641e9f745fedb50
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-01-10 10:23:04 +0100

    Release 3.0.20 - Add custom guzzle client configuration support

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.0.19";
+    const PROJECT_VERSION = "3.0.20";
 
     /**
      * @param DateTime $date

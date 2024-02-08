0195aa0153b79b3f8350509e54a5654e57f62bd3
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-02-08 18:30:23 +0100

    Release 3.0.21 (#235)
    - Adds `payment sessions` support
    - Update `payment request` for `payment contexts`
    - Update `giropay` source
    - Update properties and tests

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.0.20";
+    const PROJECT_VERSION = "3.0.21";
 
     /**
      * @param DateTime $date

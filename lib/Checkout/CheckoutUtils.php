dc1b71009f2456cabde720ee38d225c7e177adfb
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-03-26 13:27:04 +0100

    Release 3.1.0 (#246)
    - Adds subdomain for merchants
    - Adds new field challenge_notification_url to the completion object
    - Adds disabled_payment_methods in HostedPaymentSessionsRequest
    - Adds compiled submitted evidence endpoint to disputes
    - Update payment contexts
    - Update onboard entity request
    - Fix payment context details response
    - Fix getBody on null

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.0.21";
+    const PROJECT_VERSION = "3.1.0";
 
     /**
      * @param DateTime $date

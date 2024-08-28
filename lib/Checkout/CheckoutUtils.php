692a1ead7a668eec5892241b7c2f6f1d30eac6c7
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-08-28 15:03:31 +0200

    Release 3.2.4
    - Updates DNS Merchant regular expression length

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.2.3";
+    const PROJECT_VERSION = "3.2.4";
 
     /**
      * @param DateTime $date

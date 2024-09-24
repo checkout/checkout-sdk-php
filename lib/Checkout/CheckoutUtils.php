b1374388943d65aaef92144bda828047715287a8
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-09-24 16:01:42 +0200

    Update CheckoutUtils.php

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "3.2.4";
+    const PROJECT_VERSION = "3.2.5";
 
     /**
      * @param DateTime $date

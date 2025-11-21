698431de698bb307324a4921bdeb69afa50750e7
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2025-11-21 12:43:55 +0100

    Release 4.0.1 (#292)
    - Adds support to Flow endpoints

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "4.0.0";
+    const PROJECT_VERSION = "4.0.1";
 
     /**
      * @param DateTime $date

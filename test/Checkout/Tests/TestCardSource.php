965022fd13591980a98ef8f0035fde59addadbba
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2026-01-28 11:58:10 +0100

    Subdomain added for access validation endpoint (#299)
    Co-authored-by: david ruiz <david.ruiz@checkout.com>

--- test/Checkout/Tests/TestCardSource.php
+++ test/Checkout/Tests/TestCardSource.php
@@ -7,6 +7,9 @@ class TestCardSource
     public static $VisaName = "Mr. Test";
     public static $VisaNumber = "4242424242424242";
     public static $VisaExpiryMonth = 12;
-    public static $VisaExpiryYear = 2025;
+    public static $VisaExpiryYear = ""; // Will be set to current year
     public static $VisaCvv = "100";
 }
+
+// Initialize the current year when the class is loaded
+TestCardSource::$VisaExpiryYear = (int)date('Y');

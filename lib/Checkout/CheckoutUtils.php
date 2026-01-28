4e31df15b7b8e1c1c73c67cf86b120c8dd51828d
david-ruiz-cko <david.ruiz@checkout.com>
2026-01-28 19:34:35 +0100

    Release 4.2.0 - Merchant-specific DNS names support for access (#300)

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "4.1.0";
+    const PROJECT_VERSION = "4.2.0";
 
     /**
      * @param DateTime $date

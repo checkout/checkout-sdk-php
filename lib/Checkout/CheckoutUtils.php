ec20d115001bc0358cc52c6a0f2e2d62faa793ee
david ruiz <david.ruiz@checkout.com>
2026-03-25 12:39:09 +0100

    Release - 4.5.0

--- lib/Checkout/CheckoutUtils.php
+++ lib/Checkout/CheckoutUtils.php
@@ -9,7 +9,7 @@ class CheckoutUtils
 {
 
     const PROJECT_NAME = "checkout-sdk-php";
-    const PROJECT_VERSION = "4.4.1";
+    const PROJECT_VERSION = "4.5.0";
 
     /**
      * @param DateTime $date

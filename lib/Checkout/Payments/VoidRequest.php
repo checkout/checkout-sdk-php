3b2ff53e020c976144ff48212089c1580a270d54
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2026-08-25 11:37:58 +0200

    feat: add optional amount to VoidRequest for partial voids (INT-1689)

--- lib/Checkout/Payments/VoidRequest.php
+++ lib/Checkout/Payments/VoidRequest.php
@@ -5,6 +5,13 @@ namespace Checkout\Payments;
 class VoidRequest
 {
     /**
+     * The amount to void. If not specified, the full payment amount is voided.
+     * Min 0, max 9999999999.
+     * @var int
+     */
+    public $amount;
+
+    /**
      * @var string
      */
     public $reference;
